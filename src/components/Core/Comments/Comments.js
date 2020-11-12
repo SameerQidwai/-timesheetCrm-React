9b0237c2d930195ef3f3b5b871ffdb96583b10b6
Sameer <you@example.com>
2020-11-12 14:59:22 +0500

    attachment in comment check

--- src/components/Core/Comments/Comments.js
+++ src/components/Core/Comments/Comments.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-import { Row, Col, Comment, Tooltip, Avatar, Popconfirm, Form, Input, List } from 'antd'
+import { Row, Col, Comment, Tooltip, Avatar, Popconfirm, Form, Input, List, Space, Upload } from 'antd'
 
 import moment from 'moment';
 
-import { DeleteOutlined, DeleteFilled, SendOutlined } from '@ant-design/icons';
+import { DeleteOutlined, DeleteFilled, SendOutlined, PaperClipOutlined } from '@ant-design/icons';
 
 import './comment.css'
 
@@ -14,9 +14,10 @@ class Comments extends Component{
   constructor (){
     super ()
     this.messagesEnd = React.createRef();
-
+    this.UploadRef =React.createRef();
     this.state= {
       isHovered:{},
+      fileList:[],
       data:[ // coments
         {
           key:0,
@@ -27,31 +28,37 @@ class Comments extends Component{
         {
           key:1,
           author: 'Musab tarq',
-          content: `Musab said he will steal Erick's chicken`,
+          content: `I am Musab Tariq`,
           date: '2020-2-10',
         },
         {
           key:2,
           author: 'Ovais raza',
-          content: `I had a great date today with a hot chick`,
+          content: `I am Ovais Raza`,
           date: '2020-3-19',
         },
         {
           key:3,
           author: 'Shabaz ',
-          content: `Hain??`,
+          content: `Shahbz here`,
           date: '2020-4-25',
         },
         {
           key:4,
           author: 'Rabi cube',
-          content: `Solving Rubik's Cube`,
+          content: `I am Rabi`,
           date: '2020-5-5',
         },
         {
           key:5,
           author: 'Me Me',
-          content: `Doing Work`,
+          content: `Project Looks Good so far`,
+          date: '2020-9-20',
+        },
+        {
+          key:5,
+          author: 'You',
+          content: `Prevented`,
           date: '2020-9-20',
         }
       ],
@@ -72,8 +79,16 @@ class Comments extends Component{
     });
   }
   handleDelete =  (key)=>{
+    console.log(key)
     this.setState({ data: this.state.data.filter(item => item.key !== key) });
-}
+  }
+
+  handelEmpty = (e) =>{ //will not allow to press enter if value is empty
+    const { value } = this.state
+    if (e.keyCode === 13 && !value) {
+      e.preventDefault();
+    }
+  }
 
   comChange = (e) =>{ // set value of a text field
     this.setState({
@@ -85,7 +100,8 @@ class Comments extends Component{
     const { value, data } = this.state // textbar code and comment data
     if (!e.shiftKey && value){ // if shift key is pressed and has value insert new Comment on press Enter
       let comment = { // comment 
-          author: 'You', 
+          key: data[data.length-1].key,
+          author: 'Han Solo', 
           content: value,
           date: moment().format(),
       }
@@ -104,10 +120,10 @@ class Comments extends Component{
     }
   }
 
-  logScroll = (e) =>{
+  logScroll = (e) =>{ 
     if (!e.target.scrollTop){ //check the value of scroll if it is not 0 don't run
       e.persist(e.target.scrollTop) // allow change of event and use it after
-      setTimeout(() => { // when to add
+      setTimeout(() => { // wait one Second befroe call the function
         const { data } = this.state 
         if(!e.target.scrollTop && data.length<500){ // check if the scroll value is still Zero
           console.log('done')
@@ -118,10 +134,17 @@ class Comments extends Component{
           console.log('Empty')
         }
       }, 1000);
+    }else if(e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight){ //check if Scroll is on bottom  
+      e.persist(e.target)
+      setTimeout(() => { // wait One second before call the function 
+        if(e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight){
+          console.log('I will get rendered now') 
+        }
+      }, 1000);
     }
   }
 
-  commentRender = (item,index,isHovered) =>{
+  commentRender = (item, index, isHovered) =>{
     return (
       <Comment
         key={index}
@@ -150,7 +173,7 @@ class Comments extends Component{
                 onMouseOver={()=>{this.deleteIcon(index,true)}}
                 onMouseLeave={()=>{this.deleteIcon(index,false)}}
               >
-                <Popconfirm  title="You Want to delete comment?" onConfirm={this.handleDelete} okText="Yes" cancelText="No">
+                <Popconfirm  title="You Want to delete comment?" onConfirm={()=>{this.handleDelete(item.key)}} okText="Yes" cancelText="No">
                   {isHovered[index]? <DeleteFilled style={styles.delIcon}/> : <DeleteOutlined/>}
                 </Popconfirm>
               </span>
@@ -161,15 +184,42 @@ class Comments extends Component{
       </Comment>
     )
   }
+  handleChange = ({ file, fileList })=>{
+    // if (file.status !== 'uploading') {
+    //   console.log({file}, {fileList});
+    // }
+    console.log(this.state.fileList)
+  }
 
   render () { 
-    const { data, isHovered, value } = this.state
+    const { data, isHovered, value, fileList } = this.state
+    const props = {
+      name: 'file',
+      action: 'https://run.mocky.io/v3/7b95dda3-4cd0-437b-b059-2a8ba1c23569',
+      onChange: this.handleChange,
+      onRemove: file => {
+        const index = this.state.fileList.indexOf(file);
+        const newFileList = this.state.fileList.slice();
+        newFileList.splice(index, 1);
+        this.setState({
+            fileList: newFileList,
+        });
+      },
+      beforeUpload: file =>{
+        console.log('upload',file)
+        this.setState({
+          fileList: [...fileList, file]
+        })
+        return true;
+      },
+      fileList: fileList,
+    }
+    
     return (
       <Row justify="space-around">
         <Col 
           style={styles.cSec}
           span={24} 
-          order={1}
           onScroll={this.logScroll}
         >
           <List
@@ -183,11 +233,19 @@ class Comments extends Component{
           />
           {/* to scroll till down when new text is done */}
           <div ref={this.messagesEnd} /> 
-          </Col>
-        <Col span={23} style={styles.cBox} order={3}>
+        </Col>
+        <Col span={23} style={styles.cBox} >
             <Form.Item 
               colon={false}
-              label={ <Avatar alt='A' style={styles.sendAvatar}> A </Avatar>}
+              // label={ <Avatar alt='A' style={styles.sendAvatar}> A </Avatar>}
+              label={ 
+                  <PaperClipOutlined 
+                    style={ isHovered['attachIcon'] ? styles.attachIconHoverd : styles.sendIcon } 
+                    onMouseOver={()=>{this.deleteIcon('attachIcon',true)}}
+                    onMouseOut={()=>{this.deleteIcon('attachIcon',false)}}
+                    onClick={()=>{this.UploadRef.current.click()}}
+                  />
+              }
             >
               <TextArea 
                 placeholder="Enter Your Comment...." 
@@ -195,22 +253,39 @@ class Comments extends Component{
                 // allowClear
                 onPressEnter={this.addComent}
                 onChange={this.comChange}
+                onKeyDown={this.handelEmpty}
                 value={value}
               />
+              <Upload 
+                {...props}
+                style={{backgroundColor:'rosybrown'}}
+              >
+                <PaperClipOutlined ref={this.UploadRef} style={{fontSize:0}}/>
+              </Upload>
             </Form.Item>
           </Col>
-          <Col span="1" style={styles.sendBox} order={4}>
-            <Form.Item>
+          <Col span="1"  style = {{alignSelf: "flex-end", 
+            maxHeight:'100px',
+            backgroundColor:'yellow',
+            marginBottom:20 * fileList.length
+          }}>
+            <Form.Item >           
               <SendOutlined 
                 onClick={this.addComent} 
                 className="sendIcon" 
                 style={ isHovered['sendIcon'] ? styles.sendIconHoverd : styles.sendIcon } 
                 onMouseOver={()=>{this.deleteIcon('sendIcon',true)}}
-                onMouseLeave={()=>{this.deleteIcon('sendIcon',false)}}
+                onMouseOut={()=>{this.deleteIcon('sendIcon',false)}}
                 /> 
             </Form.Item>
           </Col>
-        {/* <Col span={1}> <SendOutlined/> </Col> */}
+          {/* <Col span="23">
+            <Upload 
+                {...props}
+              >
+                <PaperClipOutlined ref={this.UploadRef} style={{fontSize:0}}/>
+              </Upload>
+          </Col> */}
       </Row>
     );
   }
@@ -244,16 +319,26 @@ const styles = {
     paddingLeft: "40px"
   },
   sendBox: {
-    alignSelf: "flex-end"
+    alignSelf: "flex-end",
+    maxHeight:'100px',
+    backgroundColor:'yellow'
   },
+
   sendIcon: {
     fontSize: "24px",
     paddingLeft: "10px",
   },
+
   sendIconHoverd:{
     fontSize: "24px",
     paddingLeft: "10px",
-    color: 'blue'
+    color: 'blue',
+  },
+
+  attachIconHoverd:{
+    fontSize: "24px",
+    paddingLeft: "10px",
+    color: '#40a9ff',
   },
 
   delIcon: {
