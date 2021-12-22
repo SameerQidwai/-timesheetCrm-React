54e3827044344b56d73bf4979e9f92b1a6688ce2
sameerQidwai <sameerQidwai@gitlab.com>
2021-12-22 17:19:45 +0500

    git add command ran this time

---
+++ build/precache-manifest.aeb97513fca30c8fd38a43c79b12d76f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d33a9e3b90206d6b305813cb17445a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ce587d03b78f2482eb3",
+    "url": "/static/css/2.96eab55c.chunk.css"
+  },
+  {
+    "revision": "47c58557346c57bca3ad",
+    "url": "/static/css/main.caeea947.chunk.css"
+  },
+  {
+    "revision": "7ce587d03b78f2482eb3",
+    "url": "/static/js/2.600a2487.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/static/js/2.600a2487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c58557346c57bca3ad",
+    "url": "/static/js/main.8379b7a2.chunk.js"
+  },
+  {
+    "revision": "ac6179d0c54513e9cfa2",
+    "url": "/static/js/runtime-main.5288a424.js"
+  }
+]);
