44286793c5cdb983951465194412dde105c1d23d
sameerQidwai <sameerQidwai@gitlab.com>
2022-04-04 16:40:11 +0500

    change Base salary according to the emp Status

---
+++ build/precache-manifest.7db8e7fde0ef7b7c43e93f9dcb9376e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cefe5b1e10d5d71ba51b654f828127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ec2f22c386e31bd855",
+    "url": "/static/css/2.291b88e4.chunk.css"
+  },
+  {
+    "revision": "496774217fe5fad2a47b",
+    "url": "/static/css/main.4f65ab17.chunk.css"
+  },
+  {
+    "revision": "f1ec2f22c386e31bd855",
+    "url": "/static/js/2.b4547e93.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/static/js/2.b4547e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496774217fe5fad2a47b",
+    "url": "/static/js/main.99e90617.chunk.js"
+  },
+  {
+    "revision": "ebf56e233844f3e9d1c4",
+    "url": "/static/js/runtime-main.d1b0aef5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eee83db05184d591b79fdd804237a35f.js"
+  "/precache-manifest.7db8e7fde0ef7b7c43e93f9dcb9376e0.js"
 );
 
 self.addEventListener('message', (event) => {
