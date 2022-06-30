1d1c34de01f43e91ee56b8e43aacbf9dcfdaee57
sameerQidwai <sameerQidwai@gitlab.com>
2022-06-30 05:03:08 +0500

    bugs fixing on test

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7f301fa5153007866488f67c35654fe.js"
+  "/precache-manifest.a5ae177343f41c374a6a07c3bcec1285.js"
 );
 
 self.addEventListener('message', (event) => {
