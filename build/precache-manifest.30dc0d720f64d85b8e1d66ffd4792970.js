ab99ad07dd4a6d9fc1b7dbaed4c74b9c128c1845
Shahzaib Ahmed <thedevshezzy@gmail.com>
2022-09-24 12:59:52 +0500

    test build

---
+++ build/precache-manifest.30dc0d720f64d85b8e1d66ffd4792970.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cdf9e24bb7fe020668f6e409d7d87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904d03b983d5dc3e7544",
+    "url": "/static/css/2.c0dcd60b.chunk.css"
+  },
+  {
+    "revision": "bc0b182cfa90be67c464",
+    "url": "/static/css/main.27ebb5d1.chunk.css"
+  },
+  {
+    "revision": "904d03b983d5dc3e7544",
+    "url": "/static/js/2.0e4e6735.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/static/js/2.0e4e6735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0b182cfa90be67c464",
+    "url": "/static/js/main.ef733855.chunk.js"
+  },
+  {
+    "revision": "ebf56e233844f3e9d1c4",
+    "url": "/static/js/runtime-main.d1b0aef5.js"
+  },
+  {
+    "revision": "99dac056fac29c40b36ab9d455560efb",
+    "url": "/static/media/PowerGrotesk-Regular.99dac056.otf"
+  }
+]);
