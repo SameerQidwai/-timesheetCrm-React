0fee522c69ac45f2dbf967a4f6457d33bdf6fc77
Sameer Qidwai <samiqidwai@gmail.com>
2022-11-10 20:27:41 +0500

    start work on cost of sale

--- src/components/Core/Reports/WorkInHand.js
+++ src/components/Core/Reports/WorkInHand.js
@@ -1,14 +1,27 @@
 import { Col, Row, Table, Typography } from 'antd'
 import React, { useState, useEffect } from 'react'
-import { parseDate } from '../../../service/constant';
+import { formatCurrency, parseDate } from '../../../service/constant';
+import "../../Styles/table.css"
+import { income_revenue } from './WIHData';
 const {Title} = Typography
+
 const column = [
     {
         title: '1LM - Whole A$',
         dataIndex: 'name',
         key: 'name',
         width: 250,
-        align: 'center',
+        onHeaderCell: ()=> {
+            return {
+                className: 'whole'
+            }
+        },
+        onCell: ({className})=>{
+            return {
+                className: className
+            }
+        },
+        // align: 'center',
         fixed: 'left',
     },
     {
@@ -22,46 +35,46 @@ const column = [
     }
 ]
 
-const colData = [
-    { year: 'Jul-20', key: 'Jul-20', era: 'Actual' },
-    { year: 'Aug-20', key: 'Aug-20', era: 'Actual' },
-    { year: 'Sep-20', key: 'Sep-20', era: 'Actual' },
-    { year: 'Oct-20', key: 'Oct-20', era: 'Forecast' },
-    { year: 'Nov-20', key: 'Nov-20', era: 'Forecast' },
-    { year: 'Dec-20', key: 'Dec-20', era: 'Forecast' },
-    { year: 'Jan-21', key: 'Jan-21', era: 'Forecast' },
-    { year: 'Feb-21', key: 'Feb-21', era: 'Forecast' },
-    { year: 'Mar-21', key: 'Mar-21', era: 'Forecast' },
-    { year: 'Apr-21', key: 'Apr-21', era: 'Forecast' },
-    { year: 'May-21', key: 'May-21', era: 'Forecast' },
-    { year: 'Jun-21', key: 'Jun-21', era: 'Forecast' },
-    { year: 'FY21', key: 'FY21', era: 'Forecast' },
-];
+
   
 
 function WorkInHand() {
     const [columns, setColumns] = useState(column)
-    const [dataSource, setDataSource] = useState([...colData,...colData, ...colData,...colData])
+    const [dataSource, setDataSource] = useState([])
     
     useEffect(() => {
         creatingCol()
+        dummyStructureData()
     }, [])
 
     const creatingCol = () =>{
         let newColumns = [...columns]
         let monthColumns = []
-        let endDate = '06/30/2022'
-        for (var iDate = parseDate('07/01/2021'); iDate.isSameOrBefore(endDate); iDate.add(1, 'months')) {
+        let endDate = '06/30/2021'
+        for (var iDate = parseDate('07/01/2020'); iDate.isSameOrBefore(endDate); iDate.add(1, 'months')) {
             let el = {
               year: parseDate(iDate, 'MMM-YY'),
-              era: iDate.isBefore(parseDate(new Date()), 'month') ? 'Actual': 'Forecast',
+              era: iDate.isSameOrBefore(parseDate('09/01/2020'), 'month') ? 'Actual': 'Forecast',
             };
             monthColumns.push(monthCol(el))
         }
+        monthColumns.push(monthCol({year: 'FY21', era: 'Forcaste'}))
         newColumns[1]['children'][0]['children'] = monthColumns
         setColumns(newColumns)
+        console.log(monthColumns)
     }
 
+    const dummyStructureData = () => {
+        let totalIncome =  { className: 'total-row', name: 'TOTAL REVENUE', 'Jul-20': 0, 'Aug-20': 0, 'Sep-20': 0, 'Oct-20': 0, 'Nov-20': 0, 'Dec-20': 0, 'Jan-21': 0, 'Feb-21': 0, 'Mar-21': 0, 'Apr-21': 0, 'May-21': 0, 'Jun-21': 0, }
+        income_revenue.forEach(el=>{
+            for (var iDate = parseDate('07/01/2020'); iDate.isSameOrBefore('06/30/2021'); iDate.add(1, 'months')) {
+                let key = parseDate(iDate, 'MMM-YY')
+                totalIncome[key] += (el[key] ?? 0)
+            }
+        })
+        income_revenue.push(totalIncome)
+        setDataSource(income_revenue)
+    }
     
   return (
     <>
@@ -79,7 +92,9 @@ function WorkInHand() {
                     bordered
                     size="small"
                     pagination = {false}
+                    rowKey={(row)=> row.name}
                     columns={columns}
+                    rowClassName={(row)=> row.className}
                     dataSource={dataSource}
                     className="scroll-table fs-v-small full-width wih-report"
                     scroll={{
@@ -107,6 +122,19 @@ const monthCol = ({year, era, key})=>({
             key: year,
             width: 100,
             align: 'center',
+            onCell: ()=> {
+                return {className: year.startsWith('FY') ? 'fin-total': ''} 
+            },
+            render: (text,record) =>{
+                if(year.startsWith('FY')){
+                    let totalYear = 0
+                    for (var iDate = parseDate('07/01/2020'); iDate.isSameOrBefore('06/30/2021'); iDate.add(1, 'months')) {
+                        totalYear += record[parseDate(iDate, 'MMM-YY')] ?? 0
+                    }
+                    return totalYear ? formatCurrency(totalYear) : '-'
+                }
+                return text ? formatCurrency(text) : ''
+            }
         }
     ],
 })
