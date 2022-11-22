177b4c42e6dee8bad9724832441d24f65d856f1b
Sameer Qidwai <samiqidwai@gmail.com>
2022-11-22 19:18:58 +0500

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,30 +11,29 @@
  * See https://goo.gl/2aRDsh
  */
 
- importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
- importScripts(
-   "/precache-manifest.f895c52bd2f7833016f5696ff558c76e.js"
- );
- 
- self.addEventListener('message', (event) => {
-   if (event.data && event.data.type === 'SKIP_WAITING') {
-     self.skipWaiting();
-   }
- });
- 
- workbox.core.clientsClaim();
- 
- /**
-  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
-  * requests for URLs in the manifest.
-  * See https://goo.gl/S9QRab
-  */
- self.__precacheManifest = [].concat(self.__precacheManifest || []);
- workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
- 
- workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-   
-   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
- });
- 
+importScripts(
+  "/precache-manifest.6d2af2f2eb561d7f39d56786fd7e4f73.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
