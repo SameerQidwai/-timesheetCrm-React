8a589462f2e1896be5a24f44fa4ad7c402aa7b7e
Sameer Ahmed Qidwai <samiqidwai@gmail.com>
2023-03-16 15:55:53 +0500

    build with test-branch changes

--- build/precache-manifest.13086b83e9c11c9214fded63b6a2940e.js
+++ build/precache-manifest.13086b83e9c11c9214fded63b6a2940e.js
@@ -1,10 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<<< HEAD:build/precache-manifest.13086b83e9c11c9214fded63b6a2940e.js
     "revision": "1caa35d7451425b9347bb2d05aa95d8d",
-========
-    "revision": "30d03f9b462dcf56a376b4fcb511bb42",
->>>>>>>> de134a79b42463d7574077e83f409a934eff294a:build/precache-manifest.65d6dc4a9f5c05385e1c4c15fa153742.js
     "url": "/index.html"
   },
   {
@@ -12,11 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.c0dcd60b.chunk.css"
   },
   {
-<<<<<<<< HEAD:build/precache-manifest.13086b83e9c11c9214fded63b6a2940e.js
     "revision": "f7a0cfbe42b922de01ba",
-========
-    "revision": "47b005ce8cc4a1de20ce",
->>>>>>>> de134a79b42463d7574077e83f409a934eff294a:build/precache-manifest.65d6dc4a9f5c05385e1c4c15fa153742.js
     "url": "/static/css/main.9b9e2a91.chunk.css"
   },
   {
@@ -28,13 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.663031f7.chunk.js.LICENSE.txt"
   },
   {
-<<<<<<<< HEAD:build/precache-manifest.13086b83e9c11c9214fded63b6a2940e.js
     "revision": "f7a0cfbe42b922de01ba",
     "url": "/static/js/main.02d5327d.chunk.js"
-========
-    "revision": "47b005ce8cc4a1de20ce",
-    "url": "/static/js/main.1ceb9e12.chunk.js"
->>>>>>>> de134a79b42463d7574077e83f409a934eff294a:build/precache-manifest.65d6dc4a9f5c05385e1c4c15fa153742.js
   },
   {
     "revision": "ebf56e233844f3e9d1c4",
