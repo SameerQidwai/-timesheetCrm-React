7d38fcc853282e7b95bd70200df9830a9ea1ab15
Sameer Ahmed Qidwai <samiqidwai@gmail.com>
2023-04-20 16:35:10 +0500

    build bulktimesheet, cashflow, fles in super and bank for emp

---
+++ build/precache-manifest.50c78f8a47e808617c771f4ec7ce5788.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eed12ab561fe09214a03186220b43e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746a38a0376c46911df4",
+    "url": "/static/css/2.c0dcd60b.chunk.css"
+  },
+  {
+    "revision": "1d1bd0f0226941c56808",
+    "url": "/static/css/main.79dfc0f7.chunk.css"
+  },
+  {
+    "revision": "746a38a0376c46911df4",
+    "url": "/static/js/2.0bdd8416.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/static/js/2.0bdd8416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1bd0f0226941c56808",
+    "url": "/static/js/main.9511fb05.chunk.js"
+  },
+  {
+    "revision": "ebf56e233844f3e9d1c4",
+    "url": "/static/js/runtime-main.d1b0aef5.js"
+  },
+  {
+    "revision": "99dac056fac29c40b36ab9d455560efb",
+    "url": "/static/media/PowerGrotesk-Regular.99dac056.otf"
+  }
+]);
