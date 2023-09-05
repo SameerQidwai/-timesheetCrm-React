c72cfbe35d294dedae918af735e40b938737c1f7
Sameer Ahmed Qidwai <samiqidwai@gmail.com>
2023-09-05 20:39:55 +0500

    early stages notifitcaion forecasting unpaaid leave deduction, subcontractor and manual doh for casual

---
+++ build/precache-manifest.9a835ebf885fd15b7ebb0f6a8b451d7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c744af9bb3aa78f4b5cea251bb433cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8597fc1297e5b5f26014",
+    "url": "/static/css/2.c0dcd60b.chunk.css"
+  },
+  {
+    "revision": "853aee771f1ef001fcab",
+    "url": "/static/css/main.7c7f644c.chunk.css"
+  },
+  {
+    "revision": "8597fc1297e5b5f26014",
+    "url": "/static/js/2.b903def4.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/static/js/2.b903def4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "853aee771f1ef001fcab",
+    "url": "/static/js/main.43e180c6.chunk.js"
+  },
+  {
+    "revision": "ebf56e233844f3e9d1c4",
+    "url": "/static/js/runtime-main.d1b0aef5.js"
+  },
+  {
+    "revision": "99dac056fac29c40b36ab9d455560efb",
+    "url": "/static/media/PowerGrotesk-Regular.99dac056.otf"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f9cd43b49716d3621feb2d3f650e5f8.js"
+  "/precache-manifest.9a835ebf885fd15b7ebb0f6a8b451d7f.js"
 );
 
 self.addEventListener('message', (event) => {
