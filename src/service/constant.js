875ac0122390dba1442f82f367c62feddbd33e09
Sameer Ahmed Qidwai <samiqidwai@gmail.com>
2024-04-08 16:19:51 +0500

    gst only for admin, some bugs

--- src/service/constant.js
+++ src/service/constant.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import 'moment-weekday-calc';
 import { message as messageAlert } from 'antd';
-export const Api = 'http://localhost:3301/api/v1';
+// export const Api = 'http://localhost:3301/api/v1';
 
 // export const Api = "http://onelmcrm.gaamatech.com:8000/api/v1";
 // export const Api = "http://192.168.0.243:3000/api/v1"; // Shahzaib/
@@ -15,7 +15,7 @@ export const Api = 'http://localhost:3301/api/v1';
 // export const Api = "http://192.168.0.110:3301/api/v1"; // TrunRajPal Home
 // export const Api = "http://192.168.0.244:3301/api/v1"; // TrunRajPal Office
 
-// export const Api = "http://3.219.31.245:8000/api/v1"; //live api
+export const Api = "http://3.219.31.245:8000/api/v1"; //live api
 // export const Api = 'http://54.79.212.236:8000/api/v1'; //tower api
 // export const Api = 'https://api-towers-group.timewize.com.au/api/v1'; //towers api
 
